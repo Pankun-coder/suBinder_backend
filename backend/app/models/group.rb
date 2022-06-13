a5e17a84197ca3d079a3095486c8aba7ecfc6539
pankun-coder <jjohese@gmail.com>
2022-06-13 14:39:18 +0900

    add validation for Group

--- backend/app/models/group.rb
+++ backend/app/models/group.rb
@@ -1,3 +1,11 @@
 class Group < ApplicationRecord
+    validates :name, presence:true
+    
     has_secure_password
+    CONTAIN_NUM = /.*\d.*/
+    CONTAIN_ALPHABET = /.*[A-Za-z].*/
+    validates :password, presence: true,
+                        length: {minimum:6}
+    validates_format_of :password, with: CONTAIN_NUM
+    validates_format_of :password, with: CONTAIN_ALPHABET
 end
--- backend/test/models/group_test.rb
+++ backend/test/models/group_test.rb
@@ -1,11 +1,8 @@
 require "test_helper"
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   def setup 
-    @group = Group.new(name: "group_name", password: "password", password_confirmation: "password")
+    @group = Group.new(name: "name", password: "aaa123", password_confirmation: "aaa123")
   end
 
   test "set-up Group should be valid" do 
@@ -13,8 +10,22 @@ class GroupTest < ActiveSupport::TestCase
   end
 
   test "pw and pw_confirmation should be idnetical" do
-    @group.password_confirmation = "passwor"
+    @group.password_confirmation = "aaa1234"
     assert_not @group.save, "saved Group with not identical pw and pw_conf"
   end
 
+  test "password should not be less than 6 chars" do
+    @group.password = "a" * 4 + "1"
+    assert_not @group.save, "saved group with 5-letter-password"
+  end
+
+  test "password should not consist of only alphabets" do
+    @group.password = "a" * 6
+    assert_not @group.save, "saved group with password which does not contain numbers"
+  end
+
+  test "password should not consist of only numbers" do
+    @group.password = "1" * 6
+    assert_not @group.save, "saved group with password which does not contain alphabets"
+  end  
 end
