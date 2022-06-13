64a79c5df4bc311112b3521f7b90e49535dbe238
pankun-coder <jjohese@gmail.com>
2022-06-13 14:48:30 +0900

    add validation and test for User model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
+    validates :name, presence:true
+
+    VALID_EMAIL = /\A^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}\z/
+    validates :email, presence:true,
+                        format: { with: VALID_EMAIL },
+                        uniqueness: { case_sensitive: false }
+
     has_secure_password
+    CONTAIN_NUM = /.*\d.*/
+    CONTAIN_ALPHABET = /.*[A-Za-z].*/
+    validates :password, presence: true,
+                        length: {minimum:6}
+    validates_format_of :password, with: CONTAIN_NUM
+    validates_format_of :password, with: CONTAIN_ALPHABET
 end
--- backend/test/models/user_test.rb
+++ backend/test/models/user_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@gmail.com", password: "password123")
+  end
+
+  test "user info should be like this" do
+    assert @user.save, "did not saved set-up user"
+  end
+
+  test "should not save user without name" do
+    @user.name = ""
+    assert_not @user.save, "saved user without name"
+  end
+
+  test "should not save user without email" do
+    @user.email = ""
+    assert_not @user.save, "saved user without email"
+  end
+
+  test "should not save user without valid email" do
+    @user.email = "thisisemail"
+    assert_not @user.save, "saved user without valid email"
+  end
+
+  test "email should unique" do
+    dup_user = @user.dup
+    dup_user.email = @user.email.upcase
+    @user.save
+    assert_not dup_user.save, "saved user with un-unique email"
+  end
+
+  test "password should not be less than 6 chars" do
+    @user.password = "a" * 4 + "1"
+    assert_not @user.save, "saved user with 5-letter-password"
+  end
+
+  test "password should not consist of only alphabets" do
+    @user.password = "a" * 6
+    assert_not @user.save, "saved user with password which does not contain numbers"
+  end
+
+  test "password should not consist of only numbers" do
+    @user.password = "1" * 6
+    assert_not @user.save, "saved user with password which does not contain alphabets"
+  end  
 end
