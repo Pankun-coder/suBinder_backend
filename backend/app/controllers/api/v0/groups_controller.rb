12cd4319c28b15ba7b418f482c14fdb4a3f9aeb1
pankun-coder <jjohese@gmail.com>
2022-06-15 13:49:51 +0900

    delete unnecessary codes

--- backend/app/controllers/api/v0/groups_controller.rb
+++ backend/app/controllers/api/v0/groups_controller.rb
@@ -2,17 +2,13 @@ module Api
     module V0
         class GroupsController < ApplicationController
             def index
-                puts "test message"
-                render json: { message: "hello"}
             end
             def create
                 @group = Group.new(group_params)
                 if @group.save
                     render json: {message: "success"}
-                    puts "saved"
                 else
                     render json: {message: "fail"}
-                    puts "not"
                 end 
             end
             private
