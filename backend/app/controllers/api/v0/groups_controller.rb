1ba7a4ebd48f99343b18ce4faa97805a6e77ca5d
pankun-coder <jjohese@gmail.com>
2022-06-15 13:36:23 +0900

    create POST-new-group function

---
+++ backend/app/controllers/api/v0/groups_controller.rb
@@ -0,0 +1,24 @@
+module Api
+    module V0
+        class GroupsController < ApplicationController
+            def index
+                puts "test message"
+                render json: { message: "hello"}
+            end
+            def create
+                @group = Group.new(group_params)
+                if @group.save
+                    render json: {message: "success"}
+                    puts "saved"
+                else
+                    render json: {message: "fail"}
+                    puts "not"
+                end 
+            end
+            private
+                def group_params
+                    params.require(:group).permit(:name, :password, :password_confirmation, :password_digest)
+                end
+            end 
+    end
+end
