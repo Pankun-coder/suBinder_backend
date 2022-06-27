8519d2b74687051b016b99b949eabed4fcb8136a
pankun-coder <jjohese@gmail.com>
2022-06-27 13:24:52 +0900

    visualize class availability

--- backend/app/controllers/api/v0/class_availabilities_controller.rb
+++ backend/app/controllers/api/v0/class_availabilities_controller.rb
@@ -8,11 +8,30 @@ module Api
                 group = User.find_by(id: session[:user_id]).group
                 month_and_year = Time.zone.local(params[:year], params[:month])
                 availabilities_for_month = group.class_availabilities.where(from: month_and_year.all_month)
-                rearranged_availabilities = []
-                availabilities_for_month.each do |av| 
-                    rearranged_availabilities.push({from: {day: av.from.day, hour: av.from.hour, min: av.from.min}, to: {day: av.to.day, hour: av.to.hour, min: av.to.min}})
+                result = {}
+                Range.new(1, month_and_year.end_of_month.day).each do |day|
+                    result[day] = []
                 end
-                render json: rearranged_availabilities
+                availabilities_for_month.each do |av|
+                    if av.student
+                        result[av.from.day].push({from: {hour: av.from.hour, min: av.from.min}, to: {hour: av.to.hour, min: av.to.min}, reservedBy: av.student.id})
+                    else
+                        result[av.from.day].push({from: {hour: av.from.hour, min: av.from.min}, to: {hour: av.to.hour, min: av.to.min}, reservedBy: av.student})
+                    end
+                end
+                render json: result and return
+
+
+                result = []
+                availabilities_for_month.each do |av| 
+                    if av.student
+                        result.push({from: {day: av.from.day, hour: av.from.hour, min: av.from.min}, to: {day: av.to.day, hour: av.to.hour, min: av.to.min}, reservedBy: av.student.id})
+                    else
+                        result.push({from: {day: av.from.day, hour: av.from.hour, min: av.from.min}, to: {day: av.to.day, hour: av.to.hour, min: av.to.min}, reservedBy: av.student})
+                    end
+                end
+                render json: result
+
             end
                 
             private
