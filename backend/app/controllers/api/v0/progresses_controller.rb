4e1e4dbdc99f523860e67b63d9cfe6bc2fb9d1bd
pankun-coder <jjohese@gmail.com>
2022-07-29 11:55:26 +0900

    add front endpoint for update progress

--- backend/app/controllers/api/v0/progresses_controller.rb
+++ backend/app/controllers/api/v0/progresses_controller.rb
@@ -5,22 +5,19 @@ module Api
       before_action :is_logged_in
       def bulk_update
         params[:progresses].each do |progress_data|
-          progress = Progress.find_by(id: progress_data.id)
-          if progress.student.group != User.find_by(id: session[:userid]).group
+          progress = Progress.find_by(id: progress_data[:id])
+          if progress.student.group != User.find_by(id: session[:user_id]).group
             render json: { message: "権限のない進捗情報です" }, status: :forbidden and return
-          end
-          if progress_data.is_completed.class != "boolean"
-            render json: { message: "進捗情報が不正です" }, status: :bad_request and return
           end
         end
         params[:progresses].each do |progress_data|
-          progress = Progress.find_by(id: progress_data.id)
-          progress.is_completed = progress_data.is_completed
+          progress = Progress.find_by(id: progress_data[:id])
+          progress[:is_completed] = progress_data[:is_completed]
           if !progress.save
-            render json: { message: "進捗情報が不正です" }, status: :bad_request and return
+            render json: { message: "進捗情報が不正ですbb" }, status: :bad_request and return
           end
-          render json: { message: "正常に保存されました" }, status: :ok and return
         end
+        render json: { message: "正常に保存されました" }, status: :ok and return
       end
       def search
         if !params[:student_id]
