23567a310122796b124364bbdd80f9c08399115e
pankun-coder <jjohese@gmail.com>
2022-09-02 16:16:40 +0900

    change db settings for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-cobra = Group.create(name: "Cobra Kai", password: "aaa111")
+group = Group.create(name: "三宮プログラミング", password: "aaa111")
 
-cobra.users.create(name: "Johnny Lawrence", email: "johnny@karate.com", password: "aaa111")
+group.users.create(name: "三宮ひなこ", email: "san-no-miya@mail.com", password: "aaa111")
 
-student_names = ["Miguel Diaz", "Hawk", "Aisha Robinson"]
+student_names = %w[越谷幹夫 石橋隆介 安藤まり]
 student_names.each do |name|
-  cobra.students.create(name: name)
+  group.students.create(name: name)
 end
 
 current_time = Time.zone.now
@@ -24,8 +24,19 @@ while date <= current_time.end_of_month
     start_from = Time.zone.local(date.year, date.month, date.day, start_hour)
     ends_at = Time.zone.local(date.year, date.month, date.day, end_hour)
     5.times do
-      cobra.class_availabilities.create(from: start_from, to: ends_at)
+      group.class_availabilities.create(from: start_from, to: ends_at)
     end
   end
   date = date.tomorrow
+end
+
+course = group.courses.create(name: "JavaScript基礎")
+step_names = ["Hello World!", "データ型", "変数", "演算子", "関数"]
+step_names.each do |step|
+  course.steps.create(name: step)
+end
+
+mikio = group.students.find_by(name: "越谷幹夫")
+step_names.each do |step|
+  mikio.progresses.create(step: course.steps.find_by(name: step), is_completed: false)
 end
